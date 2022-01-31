701f7e01c73d02d46c10fa6b2825f5c88b22aa72
Ricardo Ferreira <ricardoferreirades@gmail.com>
2022-01-31 18:45:43 +0100

    refactor: add repository list component
    Signed-off-by: Ricardo Ferreira <ricardoferreirades@gmail.com>

---
+++ src/components/RepositoryList/index.jsx
@@ -0,0 +1,38 @@
+export function RepositoryList() {
+    return (
+        <section>
+            <h1>Repository List</h1>
+
+            <li>
+                <strong>Unform</strong>
+                <p>Repository description</p>
+                <a href="">Access repository</a>
+            </li>
+            <li>
+                <strong>Unform</strong>
+                <p>Repository description</p>
+                <a href="">Access repository</a>
+            </li>
+            <li>
+                <strong>Unform</strong>
+                <p>Repository description</p>
+                <a href="">Access repository</a>
+            </li>
+            <li>
+                <strong>Unform</strong>
+                <p>Repository description</p>
+                <a href="">Access repository</a>
+            </li>
+            <li>
+                <strong>Unform</strong>
+                <p>Repository description</p>
+                <a href="">Access repository</a>
+            </li>
+            <li>
+                <strong>Unform</strong>
+                <p>Repository description</p>
+                <a href="">Access repository</a>
+            </li>
+        </section>
+    )
+}
